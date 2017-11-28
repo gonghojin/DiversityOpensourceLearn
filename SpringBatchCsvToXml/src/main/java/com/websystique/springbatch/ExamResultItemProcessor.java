b7bbc37d5a3d25715d9d3251def10c721fcdda60
gong <yolip51@naver.com>
2017-11-28 12:20:13 +0900

    SpringBatch사용법 1(Csv파일을 XML파일로)

---
+++ SpringBatchCsvToXml/src/main/java/com/websystique/springbatch/ExamResultItemProcessor.java
@@ -0,0 +1,22 @@
+package com.websystique.springbatch;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.websystique.springbatch.model.ExamResult;
+
+public class ExamResultItemProcessor implements ItemProcessor<ExamResult, ExamResult> {
+
+	@Override
+	public ExamResult process(ExamResult result) throws Exception {
+		  System.out.println("Processing result :"+result);
+		  
+		  //60 이상만 리턴
+		  if(result.getPercentage() < 60){
+			  return null;
+		  }
+		  
+		  return result;
+
+	}
+
+}
