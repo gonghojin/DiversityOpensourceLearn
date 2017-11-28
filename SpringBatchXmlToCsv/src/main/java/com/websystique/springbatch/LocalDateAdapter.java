c17f187702a220d3715d973155b1c9e63cfd28df
gong <yolip51@naver.com>
2017-11-28 12:22:45 +0900

    SpringBatch사용법2(Xml파일을 csv파일로)

---
+++ SpringBatchXmlToCsv/src/main/java/com/websystique/springbatch/LocalDateAdapter.java
@@ -0,0 +1,19 @@
+package com.websystique.springbatch;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import org.joda.time.LocalDate;
+
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+	@Override
+	public String marshal(LocalDate v) throws Exception {
+		return v.toString();
+	}
+
+	@Override
+	public LocalDate unmarshal(String v) throws Exception {
+		return new LocalDate(v);
+	}
+
+}
