c236a4ac2b37a579ab8d758f3842b54eee094d2b
gong <yolip51@naver.com>
2017-12-04 15:28:03 +0900

    Srping4MVCFormValidation-messagesource 진행 중

--- Spring4MVCFormValidationExample/src/main/java/com/websystique/springbatch/controller/HelloWorldController.java
+++ Spring4MVCFormValidationExample/src/main/java/com/websystique/springbatch/controller/HelloWorldController.java
@@ -1,10 +1,14 @@
 package com.websystique.springbatch.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,29 +17,66 @@ import com.websystique.springbatch.model.Student;
 @Controller
 @RequestMapping("/")
 public class HelloWorldController {
-	
-	//기본적인 Get handler 역할
+
+	// 기본적인 Get handler 역할
 	@RequestMapping(method = RequestMethod.GET)
-	public String newRegistration(ModelMap model){
-		
+	public String newRegistration(ModelMap model) {
+
 		Student student = new Student();
 		model.addAttribute("student", student);
-		
+
 		return "enroll";
 	}
-	
+
 	/*
 	 * form submission에 호출될 메소드
 	 */
 	@RequestMapping(method = RequestMethod.POST)
-	public String saveRegistration(@Valid Student student, BindingResult result, ModelMap model){
-		
-		if(result.hasErrors()){
+	public String saveRegistration(@Valid Student student, BindingResult result, ModelMap model) {
+
+		if (result.hasErrors()) {
 			return "enroll";
 		}
-		
+
 		model.addAttribute("success", "Dear " + student.getFirstName() + ",your Registration completed successful");
 		return "success";
-		
+
 	}
+
+	@ModelAttribute("sections")
+	public List<String> initializeSections() {
+
+		List<String> sections = new ArrayList<String>();
+		sections.add("Graduate");
+		sections.add("Post Graduate");
+		sections.add("Research");
+		return sections;
+	}
+
+	@ModelAttribute("countries")
+	public List<String> initializeCountries() {
+
+		List<String> countries = new ArrayList<String>();
+		countries.add("USA");
+		countries.add("CANADA");
+		countries.add("FRANCE");
+		countries.add("GERMANY");
+		countries.add("ITALY");
+		countries.add("OTHER");
+		return countries;
+	}
+
+	@ModelAttribute("subjects")
+	public List<String> initializeSubjects() {
+
+		List<String> subjects = new ArrayList<String>();
+		subjects.add("Physics");
+		subjects.add("Chemistry");
+		subjects.add("Life Science");
+		subjects.add("Political Science");
+		subjects.add("Computer Science");
+		subjects.add("Mathmatics");
+		return subjects;
+	}
+
 }
