34c2543c39723b2582d0894ba5645592cb30cf51
gonghojin <yolip51@naver.com>
2018-06-08 17:46:15 +0900

    Feat : 시큐리티 로그아웃 로직 보충 설명(SpringSecurityCustomLoginFormAnnotationExample)

--- SpringSecurityCustomLoginFormAnnotationExample/src/main/java/com/gongdel/springsecurity/controller/HelloWorldController.java
+++ SpringSecurityCustomLoginFormAnnotationExample/src/main/java/com/gongdel/springsecurity/controller/HelloWorldController.java
@@ -46,13 +46,23 @@ public class HelloWorldController {
         return "login";
     }
 
+    /**
+     * 먼저, SecurityContextHolder.getContext().getAuthentication()을 사용하기 전에 인증된 user가 있는지 알아야 한다.
+     * 만약 있다면 SecurityContextLogoutHandler().logout(request, response, auth)를 호출하여 user 속성을 logout한다.
+     * 이 'logout' 호출은 다음의 과정을 이행한다.
+     * 1. HTTP Session을 무효화한다. 그다음 bound된 모든 객체를 unbound한다.
+     * 2. 동시 요청 문제를 예방하기 위해 SecurityContext로부터 인증을 제거한다.
+     * 3. 현재 tread로부터 context 값을 제거한다.
+     * <p>
+     * 밑에 메소드 외에 어떠한 설정도 필요하지 않음(심지어 SecurityConfiguration에서조차도 X)
+     **/
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
-        return "redirect:/login?logout";
+        return "redirect:/login?logout"; // 원하는 어디든지 redirect할 수 있지만, 일반적으로 login 스크린을 다시 보여주는 게 관행
     }
 
 
